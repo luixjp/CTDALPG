8c926b3bd4db8f83699b7d9bd8011bba23d2f848
Luis <luixjp@gmail.com>
2024-06-25 13:51:40 -0300

    Add files via upload

---
+++ Codigos/funcao2/funcao_somar.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void somar(x, y) {
+
+    int res=0;
+    res = x + y;
+    printf("O resultado da soma eh: %d\n", res);
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+    int x, y;
+
+    printf("Bem vindo ao programa de somar dois numeros inteiros!\n");
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &x);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &y);
+
+    somar(x, y);
+
+    return 0;
+}
